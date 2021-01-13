58c1442d6b27b1a18c053e53d10317238a4567be
janeDoe7 <50467708+janeDoe7@users.noreply.github.com>
2021-01-13 12:27:43 -0500

    Added add_light and set_ambient_light_mult to environment.py. Fixed exceptions in apps.

--- Apps/hvacLocationControl.py
+++ Apps/hvacLocationControl.py
@@ -9,8 +9,10 @@ class HVACLocationControl(App):
         room = sys.sensors["User Locator"].get_value()
         if not room.startswith("outside"):
             # Compute distance vs target temperature time to determine target heating/cooling rate
-            hvac_props_h = [sys.devices["HVAC_%s" % room].get_resource_usage("heating", {"rate": x + 1}) for x in range(4)]
-            hvac_props_c = [sys.devices["HVAC_%s" % room].get_resource_usage("cooling", {"rate": x + 1}) for x in range(4)]
+            hvac_props_h = \
+                [sys.devices["HVAC_%s" % room].get_resource_usage("heating", {"rate": x + 1}) for x in range(4)]
+            hvac_props_c = \
+                [sys.devices["HVAC_%s" % room].get_resource_usage("cooling", {"rate": x + 1}) for x in range(4)]
 
         actions = []
         weights = []
@@ -27,8 +29,7 @@ class HVACLocationControl(App):
 
         if cur_temp > target_temp:
             # Cooling
-            hvac_tot = [-1 * (cur_temp - target_temp) /
-                        hvac_props_c[x]["temperature_delta"] for x in range(4)]
+            hvac_tot = [-1 * (cur_temp - target_temp) / hvac_props_c[x]["temperature_delta"] for x in range(4)]
 
             App.app_print("[HVAC Location Control] [HVAC] Cooling requested")
             # Adjust penalties based on time difference till target temperature
@@ -46,7 +47,7 @@ class HVACLocationControl(App):
             App.app_print("[HVAC Location Control] [HVAC] Heating requested")
             # Adjust penalties based on time difference till target temperature
             for i in range(4):
-                actions.append({"device": "HVAC", "target": "heating_%d" % (i + 1)})
+                actions.append({"device": "HVAC_%s" % room, "target": "heating_%d" % (i + 1)})
                 weights.append([hvac_props_h[i]["power"], 5 - abs(hvac_tot[i] / 30 * 3), 0])
                 alt_actions.append(i)
             return actions, weights, [], [], [], [alt_actions]
--- Apps/sleepSecurity.py
+++ Apps/sleepSecurity.py
@@ -6,9 +6,9 @@ class SleepSecurity(App):
         App.__init__(self, "Sleep Security")
 
     def update(self, sys):
-        sleep_sensors = sys.all_sensors_of_type("Sleep Sensor")
+        sleep_sensors = sys.all_sensors_of_type("Sleep Sensor_")
         for i in range(len(sleep_sensors)):
             if not sys.sensors[sleep_sensors[i]].value:
                 App.app_print("[Sleep Security] [Doors] Doors closed requested")
-                return [{"device": "Doors", "target": "closed"}], [[0, 9, 10]], [], [], [], []
+                return [{"device": "Door_%s" % sleep_sensors[i][13:], "target": "closed"}], [[0, 9, 10]], [], [], [], []
         return [], [], [], [], [], []
